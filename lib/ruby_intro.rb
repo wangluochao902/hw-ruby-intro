b374c8b92f71f70c116f7dc1611948723656660a
Luochao Wang <wangluochao@tamu.edu>
2019-09-18 13:48:58 -0500

    add codes for homework 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,60 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0; return 0; end
+  if arr.length == 1; return arr[0]; end
+  arr = arr.sort
+  arr[-2..-1].sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  require "set"
+  s = Set.new
+  for e in arr
+    if
+      s.include? e
+      return true
+    else
+      s.add(n-e)
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match(/^[^aeiou]/i) and s.match(/^[a-z]/i)? true : false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+  s.match(/^[01]*00$/) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError,
+     "invalid argument" if isbn.empty? or price<=0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    sprintf("$%2.2f", @price)
+  end
 end
